39bec0a93c8c827e9b44920eabe5cd87e2ca189f
Nohelia F <92599133+nohelia-f@users.noreply.github.com>
2022-06-20 17:09:08 -0500

    3 functions map to user input (#6)
    * add event listener for input boxes
    
    * add event listener for input boxes
    
    * camel case fix in enterButton
    
    * clean up and better variable names
    
    * Printing the soft and hard difference on the screen

--- sft-n-hrd.js
+++ sft-n-hrd.js
@@ -1,19 +1,32 @@
-const inchesToCmsConvertionFactor = 2.54
+
+// Event listener para las cajas de input
+const enterButton= document.getElementById("enterbutton");
+
+enterButton.addEventListener('click', function(){
+  let softLenght = document.getElementById("softbox").value; //Getting info from softbox
+  let hardLenght = document.getElementById("hardbox").value; //Getting info from hardbox
+  console.log(softLenght, hardLenght);
+  document.getElementById("result").innerHTML = (hardLenght - softLenght); //Printing the difference on the screen
+});
+
+/*
+
+//Conversion de pulgadas a centimetros 
+const inchesToCmsConverstionFactor = 2.54
 let softLenghtInches = 4.5;
 let hardLenghtInches = 6.75;
 
-
  function convertInchestoCms(inch){
- return inch * inchesToCmsConvertionFactor
- }
-
- function differenceBetweenSoftAndHard(hardLenght, softLenght){
-    return hardLenght - softLenght
+ return inch * inchesToCmsConverstionFactor
  }
 
  let softLenghtCms = convertInchestoCms(softLenghtInches);
  let hardLenghtCms = convertInchestoCms(hardLenghtInches);
 
+ //Diferencia entre soft y hard
+ function differenceBetweenSoftAndHard(hardLenght, softLenght){
+    return hardLenght - softLenght
+ }
 
-console.log(softLenghtCms, hardLenghtCms, 'cm');
-console.log(differenceBetweenSoftAndHard(hardLenghtCms, softLenghtCms), 'cm diff');
+console.log(differenceBetweenSoftAndHard(softLenght, hardLenght));
+*/
